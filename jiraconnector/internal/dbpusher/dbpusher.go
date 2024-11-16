ad1b1bb39b6fb1121132e379bc2787da3033405c
nutochk <kozakova.ann@mail.ru>
2024-11-16 19:22:40 +0300

    transition on pgx, add saving status changes

--- jiraconnector/internal/dbpusher/dbpusher.go
+++ jiraconnector/internal/dbpusher/dbpusher.go
@@ -1,16 +1,18 @@
 package dbpusher
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/jiraconnector/internal/dto"
 	"github.com/jiraconnector/internal/entities"
 	"github.com/jiraconnector/internal/transformer"
-	_ "github.com/lib/pq"
 )
 
 type DataBase struct {
-	Db *sql.DB
+	Db *pgxpool.Pool
 }
 
 func NewDB() (*DataBase, error) {
@@ -21,34 +23,45 @@ func NewDB() (*DataBase, error) {
 		5432,
 		"test",
 	)
-	db, err := sql.Open("postgres", conStr)
+
+	pool, err := pgxpool.Connect(context.Background(), conStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %vn", err)
+	}
+	//defer pool.Close()
+
+	pool.Ping(context.Background())
 	if err != nil {
 		return nil, err
 	}
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
-	return &DataBase{Db: db}, nil
+	return &DataBase{Db: pool}, nil
 }
 
 func saveProject(database *DataBase, project *dto.Project) {
-	database.Db.QueryRow("SELECT id from project WHERE title = $1", project.Title).Scan(&project.ID)
+	database.Db.QueryRow(context.Background(), "SELECT id from project WHERE title = $1", project.Title).Scan(&project.ID)
 	if project.ID == 0 {
-		database.Db.QueryRow("INSERT INTO project (title) VALUES($1) RETURNING id", project.Title).Scan(&project.ID)
+		database.Db.QueryRow(context.Background(), "INSERT INTO project (title) VALUES($1) RETURNING id", project.Title).Scan(&project.ID)
 	}
 }
 
 func saveAuthor(database *DataBase, author *dto.Author) {
-	database.Db.QueryRow("SELECT id from author WHERE name = $1", author.Name).Scan(&author.ID)
+	database.Db.QueryRow(context.Background(), "SELECT id from author WHERE name = $1", author.Name).Scan(&author.ID)
 	if author.ID == 0 {
-		database.Db.QueryRow("INSERT INTO author (name) VALUES($1) RETURNING id", author.Name).Scan(&author.ID)
+		database.Db.QueryRow(context.Background(), "INSERT INTO author (name) VALUES($1) RETURNING id", author.Name).Scan(&author.ID)
+	}
+}
+
+func saveStatusChanges(database *DataBase, changes *dto.StatusChanges) {
+	_, err := database.Db.Exec(context.Background(), "INSERT INTO statuschanges (issueid, authorid, changetime, fromstatus, tostatus) "+
+		"VALUES($1, $2, $3, $4, $5)", changes.IssueId, changes.AuthorId, changes.ChangeTime, changes.FromStatus, changes.ToStatus)
+	if err != nil {
+		log.Fatalf("Unable to save status changes for issue id %d: %v", changes.IssueId, err)
 	}
 }
 
 func SaveIssue(database *DataBase, issue *entities.Issue) {
 	issueTrans := transformer.IssueToDTO(issue)
-	database.Db.QueryRow("SELECT id from issue WHERE key = $1", issueTrans.Key).Scan(&issueTrans.ID)
+	database.Db.QueryRow(context.Background(), "SELECT id from issue WHERE key = $1", issueTrans.Key).Scan(&issueTrans.ID)
 	if issueTrans.ID == 0 {
 		project := transformer.ProjectToDTO(&issue.Fields.Project)
 		creator := transformer.AuthorToDTO(&issue.Fields.Creator)
@@ -61,11 +74,32 @@ func SaveIssue(database *DataBase, issue *entities.Issue) {
 		issueTrans.AuthorId = creator.ID
 		issueTrans.AssigneeId = assignee.ID
 
-		database.Db.QueryRow("INSERT INTO issue (projectid, authorid, assigneeid, key, summary, description, type,"+
+		database.Db.QueryRow(context.Background(), "INSERT INTO issue (projectid, authorid, assigneeid, key, summary, description, type,"+
 			" priority, status, createdtime, closedtime, updatedtime, timespent) VALUES($1, $2, $3, $4, $5, $6, $7, $8, "+
 			"$9, $10, $11, $12, $13) RETURNING id", issueTrans.ProjectId, issueTrans.AuthorId, issueTrans.AssigneeId, issueTrans.Key,
 			issueTrans.Summary, issueTrans.Description, issueTrans.Type, issueTrans.Priority,
 			issueTrans.Status, issueTrans.CreatedTime, issueTrans.ClosedTime, issueTrans.UpdatedTime,
 			issueTrans.TimeSpent).Scan(&issueTrans.ID)
 	}
+	for _, history := range issue.StatusChanges.Histories {
+		for i := range len(history.Items) {
+			changes := transformer.StatusChandesToDTO(&history, i)
+			changes.IssueId = issueTrans.ID
+
+			if !isStatusChangesSaved(database, &changes) {
+				authorOfChanges := transformer.AuthorToDTO(&history.Author)
+				saveAuthor(database, &authorOfChanges)
+				changes.AuthorId = authorOfChanges.ID
+				saveStatusChanges(database, &changes)
+			}
+		}
+	}
 }
+
+func isStatusChangesSaved(database *DataBase, changes *dto.StatusChanges) bool {
+	var count int
+	database.Db.QueryRow(context.Background(), "IF EXISTS (SELECT TOP 1 1 FROM statuschanges WHERE issueid=$1 AND changetime=$2)", changes.IssueId, changes.ChangeTime).Scan(count)
+	return count == 1
+}
+
+// TODO logger
